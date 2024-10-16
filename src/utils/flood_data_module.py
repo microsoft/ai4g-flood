7b17268815ed9d67020bee9659cd1d7a2234f138
Amit Misra <amitmisra@microsoft.com>
2024-10-16 14:38:25 -0700

    inference for planetary computer and local files

---
+++ src/utils/flood_data_module.py
@@ -0,0 +1,109 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+import torch
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import pystac_client
+import planetary_computer
+from torch.utils.data import DataLoader
+from lightning import LightningDataModule
+import time
+from .flood_dataset import FloodDataset
+from data.region_polygons import get_polygon
+
+class FloodDataModule(LightningDataModule):
+    def __init__(self, batch_size, workers, region, daterange, scale_factor, run_vv_only=False):
+        super().__init__()
+        self.batch_size = batch_size
+        self.workers = workers
+        self.region = region
+        self.daterange = daterange
+        self.scale_factor = scale_factor
+        self.run_vv_only = run_vv_only
+
+    def retrieve_from_planetary_computer(self, num_retries=100):
+        polygon_coords = get_polygon(self.region)
+        polygon_geometry = {"type": "Polygon", "coordinates": [polygon_coords]}
+        for _ in range(num_retries):
+            try:
+                catalog = pystac_client.Client.open(
+                    "https://planetarycomputer.microsoft.com/api/stac/v1",
+                    modifier=planetary_computer.sign_inplace,
+                )
+                search = catalog.search(
+                    collections=["sentinel-1-rtc"],
+                    intersects=polygon_geometry,
+                    datetime=f"{self.daterange[0]}/{self.daterange[1]}"
+                )
+                return search.item_collection()
+            except Exception as e:
+                print(f'Exception {e} in flood_data_module.py')
+                time.sleep(60)
+        raise Exception("Failed to retrieve data from Planetary Computer")
+
+    def get_items_keep(self, items):
+        vv_paths, vh_paths, items_keep = [], [], []
+        for item in items:
+            assets = planetary_computer.sign_item(item).assets
+            if self.run_vv_only:
+                if 'vv' not in assets:
+                    continue
+                assets['vh'] = assets['vv']
+            else:
+                if 'vv' not in assets or 'vh' not in assets:
+                    continue
+            vv_paths.append(assets['vv'].href)
+            vh_paths.append(assets['vh'].href)
+            items_keep.append(item)
+        return items_keep, vv_paths, vh_paths
+
+    def create_ref_df(self, items_keep, vv_paths, vh_paths, max_time_delta=300, max_coord_diff = 1, max_day_delta=30):
+        df = pd.DataFrame({
+            'obs_dt': [pd.to_datetime(item.properties['end_datetime']) for item in items_keep],
+            'bbox': [item.bbox for item in items_keep]
+        })
+        df['mean_long'] = df.bbox.apply(lambda x: (x[0] + x[2]) / 2)
+        df['mean_lat'] = df.bbox.apply(lambda x: (x[1] + x[3]) / 2)
+        df['obs_time'] = df.obs_dt.apply(lambda x: datetime.combine(datetime(2000, 1, 1), x.time()))
+        vv, vh, vv_ref, vh_ref = [], [], [], []
+        for i, row in df.iterrows():
+            reference_time = row.obs_time
+            timedeltas = (df['obs_time'] - reference_time).dt.total_seconds().abs()
+            dftmp = df[((row.obs_dt - df.obs_dt).dt.days < max_day_delta) & 
+                    ((row.obs_dt - df.obs_dt).dt.days >0) & 
+                    ((row.mean_long - df.mean_long).abs() < max_coord_diff) & 
+                    ((row.mean_lat - df.mean_lat).abs() < max_coord_diff) & 
+                    (timedeltas < max_time_delta)]
+            if not dftmp.empty:
+                ref_index = dftmp.obs_dt.idxmax()
+                vv.append(vv_paths[i])
+                vh.append(vh_paths[i])
+                vv_ref.append(vv_paths[ref_index])
+                vh_ref.append(vh_paths[ref_index])
+
+        return pd.DataFrame({'vv_image_path': vv, 'vh_image_path': vh, 'vv_image_pre_path': vv_ref, 'vh_image_pre_path': vh_ref})
+
+    def setup(self, stage=None):
+        items = self.retrieve_from_planetary_computer()
+        items_keep, vv_paths, vh_paths = self.get_items_keep(items)
+        self.test_df = self.create_ref_df(items_keep, vv_paths, vh_paths)
+        self.test_ds = FloodDataset(self.test_df, self.scale_factor, run_vv_only=self.run_vv_only)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_ds, batch_size=self.batch_size, num_workers=self.workers, 
+                          shuffle=False, pin_memory=True, collate_fn=self.collate_fn)
+
+    @staticmethod
+    def collate_fn(batch):
+        filenames = [item['filename'] for item in batch]
+        years = [item['year'] for item in batch]
+        months = [item['month'] for item in batch]
+        days = [item['day'] for item in batch]
+        patches = [torch.from_numpy(patch) for item in batch for patch in item['patches']]
+        orig_shapes = [item['orig_shape'] for item in batch]
+        padded_shapes = [item['padded_shape'] for item in batch]
+        crs = [item['crs'] for item in batch]
+        transform = [item['transform'] for item in batch]
+        ignore_flag = [item['ignore_flag'] for item in batch]
+        return filenames, years, months, days, orig_shapes, padded_shapes, torch.stack(patches), crs, transform, ignore_flag
---
+++ src/utils/image_processing.py
@@ -0,0 +1,87 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+import numpy as np
+import rasterio
+from rasterio.warp import reproject, Resampling
+from rasterio.transform import Affine
+
+def db_scale(x):
+    """Convert to decibel scale and shift towards middle of 0-255 range."""
+    xnew = 10 * np.log10(x)
+    xnew = xnew * 2 + 135
+    return np.clip(np.nan_to_num(xnew, 0), 0, 255)
+
+
+def pad_to_nearest(input_array, multiple, pad_indices): 
+    pad_amts = []
+    for i in range(len(input_array.shape)):
+        if i in pad_indices:
+            lendim = input_array.shape[i]
+            pad_amt = 0 if lendim % multiple == 0 else multiple - lendim % multiple 
+            pad_amt = (0, pad_amt)
+        else:
+            pad_amt = (0, 0)
+        pad_amts.append(pad_amt)
+    pad = tuple(pad_amts)
+    return np.pad(input_array, pad, mode='constant', constant_values=0)  
+
+
+def create_patches(image, patch_size, stride):  
+    height, width = image.shape[0], image.shape[1]
+    patches = []  
+    for y in range(0, height - patch_size[0] + 1, stride):  
+        for x in range(0, width - patch_size[1] + 1, stride):  
+            patch = image[y:y+patch_size[0], x:x+patch_size[1]]  
+            patch = patch.transpose(2, 0, 1)
+            patches.append(patch)  
+    
+    return patches 
+
+
+def reconstruct_image_from_patches(patches, image_size, patch_size, stride):  
+    """Reconstruct image from patches."""
+    height, width = image_size  
+    reconstructed_image = np.zeros((height, width))  
+    pixel_counts = np.zeros((height, width)) 
+    patch_counter = 0  
+    for y in range(0, height - patch_size[0] + 1, stride):  
+        for x in range(0, width - patch_size[1] + 1, stride):  
+            patch = patches[patch_counter]
+            reconstructed_image[y:y+patch_size[0], x:x+patch_size[1]] += patch
+            pixel_counts[y:y+patch_size[0], x:x+patch_size[1]] += 1
+            patch_counter += 1  
+    return reconstructed_image, pixel_counts
+
+
+def reproject_image(source, dst_crs, dst_transform, dst_shape):
+    """Reproject image to new coordinate system."""
+    destination = np.zeros(dst_shape, source.dtype)
+    reproject(
+        source=source,
+        destination=destination,
+        src_transform=source.transform,
+        src_crs=source.crs,
+        dst_transform=dst_transform,
+        dst_crs=dst_crs,
+        resampling=Resampling.nearest
+    )
+    return destination
+
+def calculate_flood_change(vv_pre, vh_pre, vv_post, vh_post, params):
+    """Calculate flood change based on VV and VH thresholds."""
+    vv_change = ((vv_post < params['vv_threshold']) & 
+                 (vv_pre > params['vv_threshold']) & 
+                 ((vv_pre - vv_post) > params['delta_amplitude'])).astype(int)
+    vh_change = ((vh_post < params['vh_threshold']) & 
+                 (vh_pre > params['vh_threshold']) & 
+                 ((vh_pre - vh_post) > params['delta_amplitude'])).astype(int)
+    
+    zero_index = ((vv_post < params['vv_min_threshold']) | 
+                  (vv_pre < params['vv_min_threshold']) | 
+                  (vh_post < params['vh_min_threshold']) | 
+                  (vh_pre < params['vh_min_threshold']))
+    
+    vv_change[zero_index] = 0
+    vh_change[zero_index] = 0
+    
+    return np.stack((vv_change, vh_change), axis=2)
